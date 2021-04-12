c600bf2823794dfe4cc86c15048fe89b37cc2f89
iraxus1 <76206932+iraxus1@users.noreply.github.com>
2021-04-12 13:31:36 +0200

    Add files via upload

---
+++ ZAD4/ZAD 1.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<body>
+<form method="POST" action="">
+<input type="text" name="liczba1" size="10">
+<select name="znak">
+<option>+</option>
+<option>-</option>
+<option>*</option>
+<option>/</option>
+</select>
+<input type="text" name="liczba2" size="10">
+<input type="submit" value="Oblicz">
+</form>
+<?php
+$liczba1 = $_POST['liczba1'];
+$liczba2 = $_POST['liczba2'];
+$znak = $_POST['znak'];
+$wynik = "";
+switch ($znak)
+{
+ case "+":
+   echo "Wynik dodawania: ".$wynik=$liczba1+$liczba2;
+   break;
+ case "-":
+   echo "Wynik dodawania: ".$wynik = $liczba1-$liczba2;
+   break;
+ case "*":
+   echo "Wynik dodawania: ".$wynik = $liczba1*$liczba2;
+   break;
+ case "/":
+   echo "Wynik dodawania: ".$wynik = $liczba1/$liczba2;
+   break;
+}
+?>
+</body>
+</head>
+</html>
---
+++ ZAD4/ZAD 2.php
@@ -0,0 +1,25 @@
+<form action="#" method="post" >
+    </br></br>
+    Data przyjazdu: <input type="date" name="beg"></br>
+    Data wyjazdu: <input type="date" name="end"></br>
+    Ilosc os: <input type="number" name="how_many"></br>
+	Imie: <input type="text" name="name"></br>
+	Nazwisko: <input type="text" name="surname"></br>
+	Adres zamieszkania: <input type="text" name="adres"></br>
+	E-mail: <input type="email" name="E-mail"></br>
+
+    <button type="submit" name="button" formmethod="post">Wyślij</button>
+
+</form>
+<?php
+if (isset($_POST['button']))
+{
+    if (isset($_POST['beg'])&&isset($_POST['end'])&&isset($_POST['how_many'])&&isset($_POST['name'])&&isset($_POST['surname'])&&isset($_POST['adres'])&&isset($_POST['E-mail']))
+    {
+        if ($_POST['beg']<$_POST['end'])
+        {
+            echo ($_POST['beg']),"</br>",($_POST['end']),"</br>",($_POST['how_many']),"</br>",($_POST['name']),"</br>",($_POST['surname']),"</br>",($_POST['adres']),"</br>",($_POST['E-mail']);
+		}	
+    }
+}
+?>
